a0eca8785ca2c06ce8ca3f1d4da105769d9563f8
adamn1225 <anoah1225@gmail.com>
2024-08-17 15:45:44 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-gray-950 opacity-50"></div>
       <div className="relative z-10 flex flex-col h-3/4">
         <LoginBanner />
-         
+         {/* test */}
           <div className="flex flex-col gap-10 justify-center items-center flex-1 ">
             <h1 className='text-3xl text-mute-200 font-extrabold font-arialBlack'>GET A SHIPPING RATE FROM EXPERTS</h1>
             <div className="flex gap-4">
