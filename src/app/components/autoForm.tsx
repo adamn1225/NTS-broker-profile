4f4a7a98255dbff04092d953b759270fa80a1d9c
anoah1225@gmail.com <anoah1225@gmail.com>
2024-08-22 14:48:56 -0400

    last update

--- src/app/components/autoForm.tsx
+++ src/app/components/autoForm.tsx
@@ -29,7 +29,7 @@ interface MyFormProps {
 const MyForm: React.FC<MyFormProps> = ({ currentStep, nextStep, prevStep, formData, handleChange, setIsSubmitted }) => {
   const formatEmailContent = (data: FormData) => {
     return `
-      New Auto Transport Lead From NTS-Broker-Profile:
+      New Auto Transport Lead From NTS-Broker-Profile - https://nts-noah.netlify.app/::
 
       Year: ${data.e_year}
       Make: ${data.e_make}
@@ -187,7 +187,7 @@ const AutoForm = () => {
   return (
     <>
       <Button className='bg-button hover:bg-amber-400 hover:text-mute-200' onClick={() => setOpenModal(true)}>
-        Construction/Heavy/Oversize Equipment
+        Auto Transport
       </Button>
       <Modal show={openModal} size="3xl" className='bg-stone-600' onClose={onCloseModal} popup>
         <Modal.Header className='bg-stone-100'/>
--- src/app/components/ltlForm.tsx
+++ src/app/components/ltlForm.tsx
@@ -32,7 +32,7 @@ interface MyFormProps {
 const MyForm: React.FC<MyFormProps> = ({ currentStep, nextStep, prevStep, formData, handleChange, setIsSubmitted }) => {
   const formatEmailContent = (data: FormData) => {
     return `
-      New LTL/FTL Lead from NTS-Broker-Profile:
+      New LTL/FTL Lead from NTS-Broker-Profile - https://nts-noah.netlify.app/::
 
       Count: ${data.count}
       Commodity: ${data.commodity}
@@ -225,7 +225,7 @@ const LtlForm = () => {
   return (
     <>
       <Button className='bg-button hover:bg-amber-400 hover:text-mute-200' onClick={() => setOpenModal(true)}>
-        Construction/Heavy/Oversize Equipment
+        LTL/FTL Shipment
       </Button>
       <Modal show={openModal} size="3xl" className='bg-stone-600' onClose={onCloseModal} popup>
         <Modal.Header className='bg-stone-100'/>
