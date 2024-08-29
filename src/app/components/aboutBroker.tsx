794dd68e2938779ee733042659faa5d105b84642
adamn1225 <anoah1225@gmail.com>
2024-08-29 11:35:46 -0400

    profile ui changes

--- src/app/components/aboutBroker.tsx
+++ src/app/components/aboutBroker.tsx
@@ -16,22 +16,22 @@ function AboutBroker(props: Props) {
             <div className='flex flex-col md:flex-row md:justify-evenly justify-center items-center text-mute-200'>
                 <div className='flex flex-col gap-2'>
                         
-                <GiArrowDunk style={{ color: '#00ffff', width: '80px', height: 'auto', position: 'relative', left: '50' }} />
+                <GiArrowDunk style={{ color: '#b18c2f', width: '80px', height: 'auto', position: 'relative', left: '50' }} />
                  <img className='w-52 h-auto self-center' src="/profile-bg-removed.png" alt="" />
                         <h3 className='text-3xl text-center font-bold font-robotoSlab tracking-wider'>MEET NOAH</h3>
-                        <div className='my-2' style={{ border: 'solid', borderColor: '#00ffff', borderRight: 'none', borderLeft: 'none', borderWidth: '1px'}}></div>
+                        <div className='my-2' style={{ border: 'solid', borderColor: '#b18c2f', borderRight: 'none', borderLeft: 'none', borderWidth: '1px'}}></div>
                     <ul className='px-2 flex flex-col items-center justify-center gap-2'>
-    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#00ffff' }} />Over 10 years experience in logistics </li>
-    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#00ffff' }} />Handles all freight types/sizes</li>
-    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#00ffff' }} />Knowledgable - problem solver</li>
+    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#b18c2f' }} />Over 10 years experience in logistics </li>
+    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#b18c2f' }} />Handles all freight types/sizes</li>
+    <li className='flex gap-1 text-md font-semibold'><FaCheckDouble style={{ color: '#b18c2f' }} />Knowledgable - problem solver</li>
     <li></li>
         </ul>
                 </div>
                  <div className='text-center text-xl p-3 pt-8 md:text-3xl flex flex-row font-bold md:w-1/2 h-full'>
-                        <div className='flex justify-start items-start md:mr-2'>    <FaQuoteLeft style={{ color: '#00ffff' }} /> </div>
+                        <div className='flex justify-start items-start md:mr-2'>    <FaQuoteLeft style={{ color: '#b18c2f' }} /> </div>
                         <p>When starting at NTS I always told everyone how eventually this company is going to be the top of it's industry, and the tremendous amount of growth that this company has shown in such a short period of time has made me trust my instincts 10 folds. </p><br />
                         
-                        <div className='flex justify-end items-end'><FaQuoteRight style={{ color: '#00ffff' }} /></div>
+                        <div className='flex justify-end items-end'><FaQuoteRight style={{ color: '#b18c2f' }} /></div>
                 </div>
             </div>
         </article>
