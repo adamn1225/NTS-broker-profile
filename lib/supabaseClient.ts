998ccd3202791df48854cc4dd0ca8222ba10e555
adamn1225 <anoah1225@gmail.com>
2024-11-22 14:50:45 -0500

    routing and env issue

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'your-supabase-url';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || 'your-supabase-key';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
