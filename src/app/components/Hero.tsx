28bfe083ea93527b7853e49db87a36b22b77ab90
adamn1225 <anoah1225@gmail.com>
2024-11-22 16:44:37 -0500

    debug

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="relative h-1/2">
             <div className="absolute inset-0 bg-form-bg bg-cover bg-center opacity-50"></div>
-            <div className="absolute inset-0 bg-gray-950 opacity-70"></div>
+            <div className="absolute inset-0 bg-zinc-800 opacity-70"></div>
             <div className="relative flex flex-col md:h-1/3 py-28 md:py-0">
                 <div className="relative flex flex-col rounded-md drop-shadow-xl md:pb-36 md:mx-96 md:gap-6 gap-10 justify-start items-center flex-1">
             
