08a49b797ec1563af72ef2c883ececc09e8b6b98
adamn1225 <anoah1225@gmail.com>
2024-11-29 13:07:47 -0500

    ui, revisions

--- src/app/broker-profile/page.tsx
+++ src/app/broker-profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import './globals.css';
+import '../globals.css';
 import AboutBroker from '@components/aboutBroker';
 import ScrollToTopButton from '@components/ScrollToTopButton';
 import BrokerTestimonials from '@components/BrokerTestimonials';
