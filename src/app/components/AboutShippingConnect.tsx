8c78038816202c9397fd8d2df73cfff833127018
adamn1225 <anoah1225@gmail.com>
2024-12-17 11:52:52 -0500

    prof pic

--- src/app/components/AboutShippingConnect.tsx
+++ src/app/components/AboutShippingConnect.tsx
@@ -8,13 +8,14 @@ function AboutShippingConnect() {
             <article className='xxs:px-4 bg-stone-950 py-12'>
                 <div className='flex flex-col md:flex-row md:justify-evenly justify-center items-center text-mute-200'>
                     <div className='flex flex-col gap-2'>
-                        
-                        <h3 className='text-3xl text-center font-bold font-mono tracking-wider'>Shipping Connect&apos;s Experience</h3>
+                        <GiArrowDunk style={{ color: '#ff5a1f', width: '80px', height: 'auto', position: 'relative', left: '50' }} />
+                        <img className='w-52 h-auto self-center' src="/nts-pro-profile.png" alt="" />
+                        <h3 className='text-3xl text-center font-bold font-mono tracking-wider'>MEET NOAH</h3>
                         <div className='my-2' style={{ border: 'solid', borderColor: '#ff5a1f', borderRight: 'none', borderLeft: 'none', borderWidth: '1px' }}></div>
                         <ul className='flex flex-col w-full items-center justify-center gap-2'>
-                            <li className='flex items-center gap-2  text-wrap text-md font-medium'>
+                            <li className='flex items-center gap-2 text-wrap text-md font-medium'>
                                 <FaCheckDouble style={{ color: '#ff5a1f', minWidth: '24px' }} />
-                                <span>15 years of logistics experience </span>
+                                <span>10 years of experience in logistics</span>
                             </li>
                             <li className='flex items-center gap-2 text-wrap text-md font-medium'>
                                 <FaCheckDouble style={{ color: '#ff5a1f', minWidth: '24px' }} />
