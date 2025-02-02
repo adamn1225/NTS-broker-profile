a31f69b798efbcb7d059e71e4dcf8e71873b055c
adamn1225 <anoah1225@gmail.com>
2025-02-01 23:25:41 -0500

    small changes;big build

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -9,10 +9,22 @@ interface BlogPost {
 }
 
 export async function GET(req: NextRequest) {
-    const client_id = process.env.CLIENT_ID; // Set a unique client ID for each site
-    const response = await fetch(`https://noetics.io/api/clientblog?client_id=${client_id}`);
-    const posts: BlogPost[] = await response.json();
-    return NextResponse.json(posts);
+    const client_id = process.env.CLIENT_ID; // Ensure CLIENT_ID is set in your environment
+    if (!client_id) {
+        return NextResponse.json({ error: "Client ID not set" }, { status: 400 });
+    }
+
+    try {
+        const response = await fetch(`https://noetics.io/api/clientblog?client_id=${client_id}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch blog posts: ${response.statusText}`);
+        }
+        const posts: BlogPost[] = await response.json();
+        return NextResponse.json(posts);
+    } catch (error) {
+        console.error('Error fetching blog posts:', error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
 }
 
 export async function POST(req: NextRequest) {
